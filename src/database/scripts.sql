378bf5437d300786fdf06c5a560200a4e999300a
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-05-25 16:18:03 -0300

    feat: scripts in sql

---
+++ src/database/scripts.sql
@@ -0,0 +1,23 @@
+/* POSTGRESQL */
+
+CREATE DATABASE phasmophobia;
+\c phasmophobia;
+
+CREATE TABLE ghosts_types (
+    name VARCHAR(35) PRIMARY KEY,
+    description TEXT
+);
+
+CREATE TABLE evidences(
+    name VARCHAR(21) PRIMARY KEY,
+)
+
+/* association table */
+
+CREATE TABLE ghosts_evidences(
+    ghost VARCHAR(35),
+    evidence VARCHAR(21),
+    PRIMARY KEY (ghost, evidence)
+    FOREIGN KEY (ghost) REFERENCES ghosts_types(name),
+    FOREIGN KEY (evidence) REFERENCES evidences(name)
+);
