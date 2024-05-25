209b96d522978140ca1abe07414af8c2ed796c85
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-05-25 16:58:30 -0300

    feat: typerRouter attached in application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const typesRouter = require('./routes/types.routes');
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use('/types', typesRouter);
 
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
