b6dae4a56430a7356ff6cf3ee146c7f8718af556
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-05-25 16:54:58 -0300

    feat: getAllTypes function

--- src/controllers/types.controller.js
+++ src/controllers/types.controller.js
@@ -2,9 +2,21 @@ const pool = require('../database/database.config');
 
 const getAllTypes = async(req, res) => {
     try {
-        const types = await pool.query('SELECT name AS type FROM ghosts_types INNER JOIN ghosts_evidences ON ghost = ghosts_types.name GROUP BY type;');
+        const types = await pool.query(`
+        SELECT ghosts_types.name AS type, STRING_AGG(ghosts_evidences.evidence, ', ') AS evidences
+        FROM ghosts_types
+        INNER JOIN ghosts_evidences ON ghosts_types.name = ghosts_evidences.ghost
+        GROUP BY ghosts_types.name;
+        `);
+
+        return types.rowCount > 0 ?
+            res.status(200).send({ total: types.rowCount, ghosts: types.rows }) :
+            res.status(200).send({ message: 'No type of ghost was registered' });
+            
     } catch(e) {
         console.log(e);
         return res.status(500).send({ message: 'Not could GET http' });
     }
 }
+
+module.exports = { getAllTypes };
