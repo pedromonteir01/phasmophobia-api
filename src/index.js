ea2e845cdf6226197292e8ee1ac9844709669e27
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-05-28 10:23:20 -0300

    feat: cors lib

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const typesRouter = require('./routes/types.routes');
 const evidencesRouter = require('./routes/evidences.routes');
+const cors = require('cors');
 
 const port = process.env.PORT;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/types', typesRouter);
 app.use('/evidences', evidencesRouter);
